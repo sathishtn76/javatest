24d5dd5a889156656dec21972c21d0c3e860088c
Sathish <spsathish942@gmail.com>
2025-02-09 22:22:15 +0530

    updated File

---
+++ src/W3School/gitverify.java
@@ -0,0 +1,40 @@
+package W3School;
+
+import java.util.Scanner;
+
+public class gitverify {
+	static Scanner sc = new Scanner(System.in);
+
+	static void showoption() {
+		String menu[] = { "1.Sign up","2.Login","3.Exit" };
+		for (String options : menu) {
+			System.out.println(options);
+		}
+
+	}
+	static void usersignup() {
+		String newuser[]= {"Enter Your First Name","Enter Your Last Name","Enter Your Mop"};
+	    String getuserdetails[]= {"Firstname","Lastname","Mob"};
+	    for(int i=0;i<newuser.length;i++) {
+	    	System.out.println(newuser[i]);
+	    	getuserdetails[i]=sc.nextLine();
+	    }
+	    System.out.println(getuserdetails[0]);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our where is my bus application");
+		System.out.println("Are you sure you want to continue(Yes/No)");
+		String userconfirm = sc.nextLine();
+		if (userconfirm.equals("Yes") || userconfirm.equals("Y") || userconfirm.equals("yes")) {
+			showoption();
+		} else {
+			System.out.println("Thankyou");
+		}
+		String choice=sc.nextLine();
+		if(choice.equals("Signup")||choice.equals("1")||choice.equals("signup")) {
+			usersignup();
+		}
+	}
+
+}
