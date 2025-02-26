e39b0e91f695055fb1412dda0cc784f8cece7ea6
Sathish <spsathish942@gmail.com>
2025-02-26 09:39:34 +0530

    New learning class

--- src/Oops/Emplyee.java
+++ src/Oops/Emplyee.java
@@ -1,52 +1,67 @@
 package Oops;
 
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Emplyee {
-	 private String name;
-	    private int age;
-	    private double salary;
 
-	    // Public getter and setter methods for name
-	    public String getName() {
-	        return name;
-	    }
+	String firstname;
+	String lastname;
+	String email;
+	int rollno;
+	static String course = "java";
+	
+	
+	Emplyee(String firstname,String lastname,String email,int rollno){
+		this.firstname=firstname;
+		this.lastname=lastname;
+		this.email=email;
+		this.rollno=rollno;
+	}
 
-	    public void setName(String name) {
-	        this.name = name;
-	    }
+	public void register() {
+       if(userdetails(firstname)) {
+    	   System.err.println("This Name is Already Exit");
+       }
+       else {
+    	   userdetails(firstname);
+       }
+	}
+	 public boolean userdetails(String firstname) {
+		 String []usernames= {"sathish","sangaiya"};
+		 String []passwords= {"sathi","sangi"};
+		 String []newusers=new String[10];
+		 for(int i=0;i<usernames.length;i++) {
+			 if(firstname.equals(usernames[i])) {
+				 return true;
+			 }
+			 else {
+				 newusers[i]=firstname;
+			 }
+		 }
+		 System.out.println("Successfully registered");
+		 
+		 return false;
+	 }
+	
 
-	    // Public getter and setter methods for age
-	    public int getAge() {
-	        return age;
-	    }
+	public void login() {
 
-	    public void setAge(int age) {
-	        if (age > 0) { // Validation can be added in setter methods
-	            this.age = age;
-	        }
-	    }
-
-	    // Public getter and setter methods for salary
-	    public double getSalary() {
-	        return salary;
-	    }
-
-	    public void setSalary(double salary) {
-	        if (salary > 0) { // Validation can be added in setter methods
-	            this.salary = salary;
-	        }
-	    }
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Emplyee emp = new Emplyee();
-        emp.setName("Sathish");
-        emp.setAge(30);
-        emp.setSalary(50000.0);
-
-        System.out.println("Employee Name: " + emp.getName());
-        System.out.println("Employee Age: " + emp.getAge());
-        System.out.println("Employee Salary: " + emp.getSalary());
-
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter Your Firstname");
+		String firstname = obj.nextLine();
+		System.out.println("Enter Your Lastname");
+		String lastname = obj.nextLine();
+		System.out.println("Enter Your Email");
+		String email = obj.nextLine();
+		System.out.println("Enter Your Rollno");
+		int rollno = obj.nextInt();
+		Emplyee emp1=new Emplyee(firstname,lastname,email,rollno);
+		emp1.register();
 	}
 
 }
